efc710453cf05254078223a94739b844118d4741
Prabhu Manchineella <prabhumanchineella@CA-PC22PM9DVP.local>
2024-03-27 17:33:43 -0400

    Run tf fmt and add configuration aliases for terraform validate workflow

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "quicksight_admin_role_arn" {
   description = "Arn of the Quicksight Admin role created"
-  value = aws_iam_role.quicksight_admin.arn
+  value       = aws_iam_role.quicksight_admin.arn
 }
 
 output "quicksight_reader_role_arn" {
   description = "Arn of the Quicksight reader role created"
-  value = aws_iam_role.quicksight_reader.arn
+  value       = aws_iam_role.quicksight_reader.arn
 }
 
 output "cur_replication_bucket_name" {
   description = "Name of the bucket created in the destination account to replicate the CUR data from management account"
-  value = aws_s3_bucket.destination_bucket.id
+  value       = aws_s3_bucket.destination_bucket.id
 }
--- versions.tf
+++ versions.tf
@@ -3,6 +3,9 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~>5.0"
+      configuration_aliases = [
+        aws.master-account
+      ]
     }
     keycloak = {
       source  = "mrparkers/keycloak"
